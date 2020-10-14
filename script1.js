dc38bb1f3b565888cbc79978de902269c7bdc4fe
venkata reddy <72429150+venkatareddych@users.noreply.github.com>
2020-10-14 20:17:28 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,23 @@
+
+
+let addbutton = document.getElementById('btn1');
+let todocontainer = document.getElementById('todocontainer');
+let inputfield = document.getElementById('inputfield');
+
+    addbutton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputfield.value;
+    todocontainer.appendChild(paragraph);
+    inputfield.value = "";
+
+    paragraph.addEventListener('click', function(){
+    paragraph.style.textDecoration = "line-through";
+    
+    })
+
+    paragraph.addEventListener('dblclick', function(){
+        todocontainer.removeChild(paragraph);
+    })
+
+})
